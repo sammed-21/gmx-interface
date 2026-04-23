f98e221e501607c5d16f5b620fb7863410fd8832
qwinndev <qwinndev@proton.me>
2026-04-23 16:36:41 +0200

    fix leverage marks

--- src/domain/synthetics/markets/utils.ts
+++ src/domain/synthetics/markets/utils.ts
@@ -17,11 +17,11 @@ import {
 } from "sdk/utils/markets";
 
 import { getCappedPositionImpactUsd } from "../fees";
+import { JitLiquidityInfo, getJitMaxReservedUsd } from "../jit/utils";
 import { PositionInfo } from "../positions";
 import { convertToTokenAmount, convertToUsd, getMidPrice } from "../tokens";
 import { isGlvAddress, isGlvInfo } from "./glv";
 import { GlvInfo, GlvOrMarketInfo, MarketInfo } from "./types";
-import { JitLiquidityInfo, getJitMaxReservedUsd } from "../jit/utils";
 import { TokenData, TokensData } from "../tokens/types";
 
 export * from "sdk/utils/markets";
@@ -455,17 +455,11 @@ export function getTradeboxLeverageSliderMarks(maxAllowedLeverage: number) {
   } else if (allowedLeverage >= 80) {
     return [0.1, 1, 2, 5, 10, 50, 80];
   } else if (allowedLeverage >= 75) {
-    return [0.1, 1, 2, 5, 10, 30, 50, 75];
-  } else if (allowedLeverage >= 70) {
-    return [0.1, 1, 2, 5, 10, 30, 50, 70];
-  } else if (allowedLeverage >= 60) {
-    return [0.1, 1, 2, 5, 10, 25, 50, 60];
+    return [0.1, 1, 2, 5, 10, 25, 50, allowedLeverage];
   } else if (allowedLeverage >= 50) {
-    return [0.1, 1, 2, 5, 10, 25, 50];
+    return [0.1, 1, 2, 5, 10, 25, allowedLeverage];
   } else if (allowedLeverage >= 30) {
-    return [0.1, 1, 2, 5, 10, 30];
-  } else if (allowedLeverage >= 25) {
-    return [0.1, 1, 2, 5, 10, 25];
+    return [0.1, 1, 2, 5, 10, allowedLeverage];
   } else if (allowedLeverage >= 10) {
     return [0.1, 1, 2, 5, allowedLeverage];
   } else {
